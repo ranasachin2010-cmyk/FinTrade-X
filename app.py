298da8d65449cf8fef62bc402653dfcb7e49f236
ranasachin2010-cmyk <ranasachin2010@gmail.com>
2026-09-17 18:55:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
-import pandas_ta as ta
+import pandas as ta
 import plotly.graph_objects as go
 from textblob import TextBlob
 import requests
